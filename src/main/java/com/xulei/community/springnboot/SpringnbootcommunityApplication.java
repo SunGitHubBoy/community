d894f3d32ab0ee235e3aa3d7048535c7c0de6380
BigSimpleBoy <xuleiCna@163.com>
2020-01-01 23:52:46 +0800

    init repo

---
+++ src/main/java/com/xulei/community/springnboot/SpringnbootcommunityApplication.java
@@ -0,0 +1,13 @@
+package com.xulei.community.springnboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringnbootcommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringnbootcommunityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xulei/community/springnboot/SpringnbootcommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xulei.community.springnboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringnbootcommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
